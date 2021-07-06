7fb0b73e1d3e01557222c23bcf85037077b7a172
bharath03-a <54313635+bharath03-a@users.noreply.github.com>
2021-07-06 12:38:47 +0530

    Add files via upload

---
+++ cloudfunction.py
@@ -0,0 +1,54 @@
+from google.auth.transport.requests import Request
+from google.oauth2 import id_token
+import requests
+
+
+IAM_SCOPE = 'https://www.googleapis.com/auth/iam'
+OAUTH_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
+
+
+def trigger_dag(data, context=None):
+  
+    client_id = '845910066070-3ikordtpsmsdalvh5mspp3jo05q4hvk3.apps.googleusercontent.com'
+    webserver_id = 'e17da18ed1891ef01p-tp'
+    # The name of the DAG you wish to trigger
+    dag_name = 'DAG-2'
+    webserver_url = (
+        'https://'
+        + webserver_id
+        + '.appspot.com/api/experimental/dags/'
+        + dag_name
+        + '/dag_runs'
+    )
+    # Make a POST request to IAP which then Triggers the DAG
+    make_iap_request(
+        webserver_url, client_id, method='POST', json={"conf": data, "replace_microseconds": 'false'})
+
+
+# This code is copied from
+# https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/iap/make_iap_request.py
+# START COPIED IAP CODE
+def make_iap_request(url, client_id, method='GET', **kwargs):
+    
+    # Set the default timeout, if missing
+    if 'timeout' not in kwargs:
+        kwargs['timeout'] = 90
+
+    # Obtain an OpenID Connect (OIDC) token from metadata server or using service
+    # account.
+    google_open_id_connect_token = id_token.fetch_id_token(Request(), client_id)
+
+    
+    resp = requests.request(
+        method, url,
+        headers={'Authorization': 'Bearer {}'.format(
+            google_open_id_connect_token)}, **kwargs)
+    if resp.status_code == 403:
+        raise Exception('Service account does not have permission to '
+                        'access the IAP-protected application.')
+    elif resp.status_code != 200:
+        raise Exception(
+            'Bad response from application: {!r} / {!r} / {!r}'.format(
+                resp.status_code, resp.headers, resp.text))
+    else:
+        return resp.text
